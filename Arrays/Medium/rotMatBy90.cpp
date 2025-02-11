1de0648c6bc78214f3f4ed2237167afc593d62ce
Ashis Ranjan Hota <ashishota83@gmail.com>
2025-02-11 15:51:04 +0530

    Added more files

--- Arrays/Medium/rotMatBy90.cpp
+++ Arrays/Medium/rotMatBy90.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class Brute {
+class Optimal {
 public:
     void rotateMatrix(vector<vector<int>> matrix) {  
         int n = matrix.size();
@@ -31,95 +31,14 @@ public:
     }
 };
 
-class Better {
-public:
-    void rotateMatrix(vector<vector<int>> matrix) {  
-        int rows = matrix.size(), cols = matrix[0].size();
-        vector<int> rowFlag(rows, 1), colFlag(cols, 1);
-        
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                if (matrix[i][j] == 0)
-                    rowFlag[i] = colFlag[j] = 0;
-
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                if (rowFlag[i] == 0 || colFlag[j] == 0)
-                    matrix[i][j] = 0;
-
-        printMatrix(matrix);  
-    }
-
-    void printMatrix(const vector<vector<int>>& matrix) { 
-        for (const auto& row : matrix) {
-            for (int elem : row) cout << elem << " ";
-            cout << endl;
-        }
-    }
-};
-
-class Optimal {
-public:
-    void rotateMatrix(vector<vector<int>> matrix) {  
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-        
-        int col0=0;
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
-                }
-            }          
-        }
-
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        
-        if(matrix[0][0]==0) {
-            for(int j=0;j<cols;j++) {
-                matrix[0][j]=0;
-            }
-        }
-        if(col0==0) {
-            for(int i=0;i<rows;i++) {
-                matrix[i][0]=0;
-            }
-        }        
-                   
-
-        printMatrix(matrix);  
-    }
-    
-    void printMatrix(const vector<vector<int>>& matrix) { 
-        for (const auto& row : matrix) {
-            for (int elem : row) cout << elem << " ";
-            cout << endl;
-        }
-    }
-};
 
 int main() {
     vector<vector<int>> matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
 
-    Brute codeA;
-    cout << "Brute Approach Output:\n";
+    Optimal codeA;
+    cout << "Output:\n";
     codeA.rotateMatrix(matrix);  
     
-    // cout << "\nBetter Approach Output:\n";
-    // Better codeB;
-    // codeB.rotateMatrix(matrix); 
-
-    // cout << "\nOptimal Approach Output:\n";
-    // Optimal codeC;
-    // codeC.rotateMatrix(matrix);
 
     return 0;
 }
--- Arrays/Medium/spiralMatrix.cpp
+++ Arrays/Medium/spiralMatrix.cpp
@@ -1,1 +1,87 @@
 #include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Brute {
+    public:
+        vector<int> spiralOrder(vector<vector<int>>& matrix) {
+            vector<int> vec;
+            int i=0,j=0,k=0;
+            int rows=matrix.size();
+            int cols=matrix[0].size();
+
+            int ub_row=0;
+            int db_row=rows-1;
+            int lb_col=0;
+            int rb_col=cols-1;
+
+            while(k!=(rows*cols)) {
+                while(j<=rb_col && k!=(rows*cols)) {
+                    vec.push_back(matrix[i][j]);
+                    k++;
+                    j++;
+                }
+                if(k==(rows*cols)) return vec;
+                rb_col--;
+                j--;
+                i++;
+                while(i<=db_row && k!=(rows*cols)) {
+                    vec.push_back(matrix[i][j]);
+                    k++;
+                    i++;
+                }
+                if(k==(rows*cols)) return vec;
+                db_row--;
+                i--;
+                j--;
+                while(j>=lb_col && k!=(rows*cols)) {
+                    vec.push_back(matrix[i][j]);
+                    k++;
+                    j--;
+                }
+                if(k==(rows*cols)) return vec;
+                lb_col++;
+                j++;
+                i--;
+                while(i>ub_row && k!=(rows*cols)) {
+                    vec.push_back(matrix[i][j]);
+                    k++;
+                    i--;
+                }
+                if(k==(rows*cols)) return vec;
+                ub_row++;
+                i++;
+                j++;
+                
+            }
+
+            return vec; 
+        }
+
+        void print(const vector<int>& vec) { 
+            for (auto row : vec) {
+                cout << row << " ";
+            }
+            cout << endl;
+        }
+};
+
+int main() {
+    vector<vector<int>> matrix = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+
+    Brute codeA;
+    cout << "Brute Approach Output:\n";
+    vector<int> res=codeA.spiralOrder(matrix);  
+    codeA.print(res);
+    
+    // cout << "\nBetter Approach Output:\n";
+    // Better codeB;
+    // codeB.rotateMatrix(matrix); 
+
+    // cout << "\nOptimal Approach Output:\n";
+    // Optimal codeC;
+    // codeC.rotateMatrix(matrix);
+
+    return 0;
+}
